ca33bc11cc4fd2c5b1d118e0fa2c0bf00754e42a
yosshi4486 <yoshiharuseijin@gmail.com>
2020-12-14 17:43:34 +0900

    feat: Add viewControllers.

---
+++ MultiColumnUndoSample/DetailViewController.swift
@@ -0,0 +1,12 @@
+//
+//  DetailViewController.swift
+//  MultiColumnUndoSample
+//
+//  Created by yosshi4486 on 2020/12/14.
+//
+
+import UIKit
+
+final class DetailViewController : UITableViewController {
+
+}
---
+++ MultiColumnUndoSample/PrimaryViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PrimaryViewController.swift
+//  MultiColumnUndoSample
+//
+//  Created by yosshi4486 on 2020/12/14.
+//
+
+import UIKit
+
+final class PrimaryViewController : UITableViewController {
+
+}
